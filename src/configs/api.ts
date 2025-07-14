e1099a13bf7ebe5cfc5158439b486121edebd6ec
unknown <nhatthach2703@gmail.com>
2025-07-14 21:32:31 +0700

    update api

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,3 +1,3 @@
 // API Configuration
-export const API_BASE_URL = "http://3.93.248.130:5000"; // Replace with your actual API base URL
+export const API_BASE_URL = "http://3.93.239.130:5000"; // Replace with your actual API base URL
 export const API_TIMEOUT = 50000;
